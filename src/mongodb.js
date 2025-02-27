63c16f01ddab84c05b9c1eb2de0e4368cd7a1b35
NathanL014 <138946052+NathanL014@users.noreply.github.com>
2025-02-27 16:39:07 +0800

    used dotenv for mongodb

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
+require('dotenv').config()
 
-mongoose.connect("mongodb://localhost:27017/ThesisV1")
+mongoose.connect(process.env.MONGO_DB_URL)
 .then(() => {
     console.log("MONGODB CONNECTED")
 })
